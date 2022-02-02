9468b78ad77694077d3c8fe36abf5572e4f41ef7
Sarah Grant <sgrant1295@gmail.com>
2022-02-02 10:48:38 -0500

    Created variables and types projects

--- 00_HelloWorld/Program.cs
+++ 00_HelloWorld/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, I like cats");
-Console.WriteLine("Hi, I'm Sarah");
-CrossAppDomainDelegate
+﻿string firstName = "Sarah";
+Console.WriteLine(firstName);
+
+Console.WriteLine()
---
+++ 01_Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What is your first name?");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("You said your first name is " + firstName);
+
+Console.WriteLine("What is your favorite color?");
+string color = Console.ReadLine();
+
+Console.WriteLine("You said your favorite color is " + color);
