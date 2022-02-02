de957b373f6e97c3c96523c22270d9096a78ced4
Sarah Grant <sgrant1295@gmail.com>
2022-02-02 11:12:31 -0500

    Adding 02_Types to Fundamentla Solution/Project

--- 02_Types/Program.cs
+++ 02_Types/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Bool is a true false integer only, no numbers or other characters
+// True and false are keywords, not strings
+bool isLoggedIn = true;
+Console.WriteLine (isLoggedIn);
+
+// Integers are whole numbers only
+int age = 42;
+Console.WriteLine(age);
+
+double balanceDue = 800.99d;
+Console.WriteLine(balanceDue);
+
+// Decimal can have a lot of possible decimal points
+
+decimal accountBalance = 9000.01m;
+Console.WriteLine(accountBalance);
+
+float averageHeight = 5.9f;
+Console.WriteLine(averageHeight);
