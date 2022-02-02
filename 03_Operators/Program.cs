d35264bfb490d18fbcd6ff53f5899ca4efdbc9e8
Sarah Grant <sgrant1295@gmail.com>
2022-02-02 12:06:10 -0500

    Added 03_Operators to Fundamental Solution/Project

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿int additionResult = 5 + 2;
+Console.WriteLine("5 + 2 = " + additionResult);
+
+int subtractionResult = 5 - 2; 
+Console.WriteLine("5 - 2 = " + subtractionResult);
+
+int multiplicationResult = 5 * 2;
+Console.WriteLine("5 * 2 = " + multiplicationResult);
+
+double divisionResult = 5d / 2d;
+Console.WriteLine("5 / 2 = " + divisionResult);
+
+// This is the shorthand version of the above
+// int numOne = 5;
+// int numTwo = 2; 
+// int result = numOne + numTwo;
+
+Console.WriteLine(10 % 2);
+Console.WriteLine(10 % 9);
+Console.WriteLine(16 % 3);
+Console.WriteLine(19 % 5);
