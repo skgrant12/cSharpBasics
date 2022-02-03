47668b12b89663b002dd310a700c92ed740a19f4
Sarah Grant <sgrant1295@gmail.com>
2022-02-03 10:32:04 -0500

    Adding Ternaries to Fundamentals Solution/Project

---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your age:");
+string response = System.Console.ReadLine();
+
+//if the string can be parsed into a NUMBER, please do it
+int age = int.Parse(response);
+
+//Computer gives us a prompt, if we answer something > or  = the number, it's true, so we get "you can vote" 
+//If what we put in is false, or < 18, then we get the second response, "You're too young to vote!"
+string output = age >= 18 ? "You can vote!" : "You're too young to vote!";
+Console.WriteLine(output);
+
